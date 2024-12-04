f815ac1335654be7e63b3573a18b68dec033fdd7
Megh39 <meghnanavati1@gmail.com>
2024-12-05 02:11:25 +0530

    Update CertificationAndClient.jsx

--- src/SEDOComponents/Components/CertificationAndClient/CertificationAndClient.jsx
+++ src/SEDOComponents/Components/CertificationAndClient/CertificationAndClient.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React,{useEffect} from "react";
 import SeedoCertification from "../Certification/SeedoCertification";
 import Client from "../Clients/Clients";
 import { Link } from "react-router-dom";
 const CertificationAndClient = () => {
+    useEffect(() => {
+        // Scroll to the top of the page when the component is mounted
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <>
             <section id="CertificationAndClient">
