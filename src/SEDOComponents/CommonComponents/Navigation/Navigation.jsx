101836b2a071575dd356089a6ff09d9aa5ee117b
Megh39 <meghnanavati1@gmail.com>
2024-12-05 10:22:06 +0530

    some update

--- src/SEDOComponents/CommonComponents/Navigation/Navigation.jsx
+++ src/SEDOComponents/CommonComponents/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavigationLinks from "./NavigationLinks";
 import "../../Styles/Navigation.css"
 import SEEDoLogo from "../../../../public/SEEDOLogo.png"
@@ -8,18 +8,47 @@ import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const [isLogoVisible, setLogoVisible] = useState(false); // Track visibility of Navigation Logo
+
     const closeMenu = () => {
         setMobileMenuOpen(false);
     };
     const { pathname } = useLocation(); // Get the current path
     const linkColor = pathname === "/" ? "white" : "black"; // Determine color based on the current page
+    useEffect(() => {
+        if (pathname === "/") {
+            // Logo visibility animation for the homepage
+            const scrollingLogoContainer = document.querySelector(".ScrollingLogoContainer");
+
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    setLogoVisible(entry.isIntersecting);
+                },
+                { threshold: 0.6 } // Trigger when 60% of the element is visible
+            );
+
+            if (scrollingLogoContainer) {
+                observer.observe(scrollingLogoContainer);
+            }
+
+            return () => {
+                if (scrollingLogoContainer) {
+                    observer.unobserve(scrollingLogoContainer);
+                }
+            };
+        } else {
+            // Ensure logo is always visible on other pages
+            setLogoVisible(true);
+        }
+    }, [pathname]);
 
     return (
         <>
             <section id="NavigationContainer">
                 <div className="ContainerNavigation">
                     <div>
-                        <div className="NavigationLogo">
+                        <div className={`NavigationLogo ${pathname === "/" && !isLogoVisible ? "visible" : "hidden"}`}
+                        >
                             <Link to="/"> <img src={SEEDoLogo} alt="" /></Link>
                         </div>
                     </div>
