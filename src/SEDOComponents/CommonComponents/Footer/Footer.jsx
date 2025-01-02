a2c52e26643fc8a4c33aacd5488946ef44ddb13b
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-01-02 13:37:25 +0530

    Update Footer

--- src/SEDOComponents/CommonComponents/Footer/Footer.jsx
+++ src/SEDOComponents/CommonComponents/Footer/Footer.jsx
@@ -13,11 +13,12 @@ const Footer = () => {
     return (
         <>
             <section id="Footer">
-                <div>
+                <h1>GET IN TOUCH</h1>
+                {/* <div>
                     <Row>
                         <Col lg={10} md={24}>
                             <div className="FooterLogoContainer" style={{ paddingLeft: "0px" }}>
-                                {/* <Link to="/"><img src={SEEDOLogo} alt="" /></Link> */}
+                              
 
 
 
@@ -41,7 +42,7 @@ const Footer = () => {
                             <div className="FooterLinksContainer">
                                 <h2>Links</h2>
                                 <div className="footerNavigation">
-                                    {/* <a>Home</a> */}
+                              
                                     <Link to="/">Home</Link>
                                     <Link to="/aboutSeedo">About Us</Link>
                                     <Link to="/certification-and-client">Certifications and Clients</Link>
@@ -64,10 +65,7 @@ const Footer = () => {
                                     <div><LocationOnIcon /><Link to="https://maps.app.goo.gl/iTpGNmjCsHpCQwYw9" target="_blank">B5/70, 1st Floor, Rama Road Industrial Area, New Delhi, India -110015.</Link></div>
                                     <div><PhoneIcon /><Link to="tel:+919599036749" target="_blank">+91 95990 36749</Link></div>
                                     <div><MailIcon /><Link to="mailto:info@seedotoys.com" target="_blank">info@seedotoys.com</Link></div>
-                                    {/* <div><IoLocationSharp  /><span>B5/70, 1st Floor, Rama Road Industrial Area, New Delhi, India -110015.</span></div> */}
-                                    {/* <div><IoMdCall /><span>+91 95990 36749</span></div> */}
-                                    {/* <div><FaEnvelope /><span>info@seedotoys.com</span></div> */}
-                                    {/* <div><span>+000-1234-55000</span></div> */}
+                                   
                                 </div>
                             </div>
                         </Col>
@@ -76,7 +74,7 @@ const Footer = () => {
                 <hr />
                 <div className="CopyWriteText">
                     <p>© 2024 <Link to="https://www.outleadsolutions.com/" target="_blank" style={{ color: "white", textDecoration: "none" }}><b>OutLead Solutions.</b></Link> All rights reserved.</p>
-                </div>
+                </div> */}
             </section>
         </>
     )
