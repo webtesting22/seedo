38e00cf27c48233caa1ac4bee8b3b01cfd16968b
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-01-09 12:54:09 +0530

    Updated the products on home page bestseller

--- src/SEDOComponents/BestSellerProductList.jsx
+++ src/SEDOComponents/BestSellerProductList.jsx
@@ -1,7 +1,15 @@
 import RC001 from "/Images/ProductsImages/RC Car/118RC001.png"
 import SC001_124 from "/Images/ProductsImages/RC Car/124SC001.png"
 import LRC00D from "/Images/ProductsImages/RC Car/124LRC00D.png"
-
+import LRC00E from "/Images/ProductsImages/RC Car/124LRC00E.png"
+import RC00127MHz from "/Images/ProductsImages/RC Car/116RC001-27MHz.png"
+import RC00124GHz from "/Images/ProductsImages/RC Car/116RC001-2.4GHz.png"
+import MT003 from "/Images/ProductsImages/RC Car/118MT003.png"
+import MT003_2 from "/Images/ProductsImages/RC Car/118MT003(2).png"
+import SMRFWPBP3 from "/Images/ProductsImages/Metal x Racer/FireBirdsSeries/SMR-FWPB-P3.png"
+import PNGSSPO9 from "/Images/ProductsImages/Friction and pull back products/PNG-SS-PO9.png"
+import PNGSSPO9_2 from "/Images/ProductsImages/Friction and pull back products/PNG-SS-PO9(2).png"
+import PNGSSPO9_3 from "/Images/ProductsImages/Friction and pull back products/PNG-SS-PO9(3).png"
 const BestSellerProducts = {
     "BestSeller": {
         products: [
@@ -34,44 +42,57 @@ const BestSellerProducts = {
 
             },
             {
-                id: 4,
-                name: "SEEDO Road Sniper 01 Car",
+                id:4,
+                name: "SEEDO Road Sniper 02 Car",
                 ProductBatteryType: "Rechargable",
                 ProductImage: [
-                    LRC00D
+                    LRC00E
                 ],
-                ProductStyleCode: "124LRC00D",
-
+                ProductStyleCode: "124LRC00E"
             },
             {
                 id: 5,
-                name: "SEEDO Road Sniper 01 Car",
+                name: "SEEDO Luxurious Sports Car 27 MHz",
                 ProductBatteryType: "Rechargable",
                 ProductImage: [
-                    LRC00D
+                    RC00127MHz
                 ],
-                ProductStyleCode: "124LRC00D",
-
+                ProductStyleCode: "116RC001/27MHz"
             },
             {
                 id: 6,
-                name: "SEEDO Road Sniper 01 Car",
+                name: "SEEDO Luxurious Sports Car 2.4GHz",
                 ProductBatteryType: "Rechargable",
                 ProductImage: [
-                    LRC00D
+                    RC00124GHz
                 ],
-                ProductStyleCode: "124LRC00D",
-
+                ProductStyleCode: "116RC001/2.4GHz"
             },
             {
                 id: 7,
-                name: "SEEDO Road Sniper 01 Car",
+                name: "SEEDO Dino/Shark/PVC Rock Crawler",
                 ProductBatteryType: "Rechargable",
                 ProductImage: [
-                    LRC00D
+                    MT003, MT003_2
                 ],
-                ProductStyleCode: "124LRC00D",
-
+                ProductStyleCode: "118MT003"
+            },
+            {
+                id: 8,
+                name: "SEEDO Metal Racers Pack of 3",
+                ProductImage: [
+                    SMRFWPBP3
+                ],
+                ProductBatteryType: "Non-Electric",
+                ProductCode: "SMR-FWPB-P3",
+            },
+            {
+                id: 9,
+                name: "Space series",
+                ProductImage: [
+                    PNGSSPO9, PNGSSPO9_2, PNGSSPO9_3
+                ],
+                ProductCode: "PNG-SS-PO9"
             },
         ]
     }
