223055ee04dbb56a65fed261dba017c8e9a10312
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-01-30 10:55:27 +0530

    Updated Sanson Section

--- src/SEDOComponents/CommonComponents/ZoomCards/ZoomCards.jsx
+++ src/SEDOComponents/CommonComponents/ZoomCards/ZoomCards.jsx
@@ -202,7 +202,7 @@ const ZoomCards = () => {
                         <h1 style={{ fontSize: "30px", color: "black", margin: "0px" }} data-aos="fade-up"
                             data-aos-duration="1000" >Sanson Promotion Innovations LLP</h1>
                         <p data-aos="fade-up"
-                            data-aos-duration="1500"> Sanson Promotion Innovations LLP is a prominent leader in consumer and trade promotions, trusted by major FMCG brands such as Unilever, Glaxo, Perfetti, Mondelez, and Britannia. As the parent company of Seedo, Sanson excels in collaborating with esteemed licensors like Disney, Marvel, Mattel, Pokémon, and Doraemon. With comprehensive expertise in conceptualization, product engineering, mass manufacturing, quality assurance, and logistics, Sanson consistently surpasses client expectations. <br /> This commitment to delivering high-quality products and innovative campaigns is reflected in Seedo's values as well.</p>
+                            data-aos-duration="1500"> <b style={{backgroundColor:"#f8c6d466"}}>Sanson Promotion Innovations LLP</b> is a prominent leader in consumer and trade promotions, trusted by major FMCG brands such as Unilever, Glaxo, Perfetti, Mondelez, and Britannia. As the parent company of Seedo, Sanson excels in collaborating with esteemed licensors like Disney, Marvel, Mattel, Pokémon, and Doraemon. With comprehensive expertise in conceptualization, product engineering, mass manufacturing, quality assurance, and logistics, Sanson consistently surpasses client expectations. <br /> This commitment to delivering high-quality products and innovative campaigns is reflected in Seedo's values as well.</p>
                     </div>
                 </div>
             </div>
