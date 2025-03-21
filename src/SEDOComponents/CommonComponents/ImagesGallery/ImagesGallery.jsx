21ca25e96e2d72bc2867659ba076257e0a31d298
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-03-21 12:26:02 +0530

    carousal autoplay in homepage

--- src/SEDOComponents/CommonComponents/ImagesGallery/ImagesGallery.jsx
+++ src/SEDOComponents/CommonComponents/ImagesGallery/ImagesGallery.jsx
@@ -3,32 +3,14 @@ import { Row, Col } from "antd";
 import "./ImagesGallery.css"
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import image1 from "./1.png"
-import image2 from "./2.png"
-import image3 from "./3.png"
-import image4 from "./4.png"
-import image5 from "./5.png"
-import image6 from "./6.png"
-import BestSeller1 from "./1.svg"
-import BestSeller2 from "./2.svg"
-import BestSeller3 from "./3.svg"
 import { Swiper, SwiperSlide } from 'swiper/react';
-import odm from "./odm.png"
-import oem from "./oem.png"
-import white from "./white label.png"
-import SeedbrandIcon from "./SeedbrandIcon.png"
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import BackColorShade from "./BackColorShade.svg"
 // import required modules
 import { Autoplay, Pagination, Navigation, FreeMode } from 'swiper/modules';
-import MakeinIndia from "./Make-in-India-Logo-PNG-HD.svg"
 import BestSellerProducts from "../../BestSellerProductList";
-import ProductsBackShape from "./ProductsBackShape.svg"
 const ImagesGallery = () => {
     const [selectedColors, setSelectedColors] = useState(
         BestSellerProducts.BestSeller.products.map(product =>
@@ -42,70 +24,9 @@ const ImagesGallery = () => {
         setSelectedColors(updatedColors);
     };
 
-
-    const ImagesGalleryData = [
-        image1,
-        image2,
-        image3,
-        image4,
-        image5,
-        image6,
-    ]
-
-    const BestSellerdata = [
-
-        // BestSeller3,
-        // BestSeller1,
-        BestSeller2,
-        BestSeller2,
-        BestSeller2,
-        BestSeller2,
-        BestSeller2,
-        BestSeller2,
-        BestSeller2,
-        BestSeller2,
-
-
-
-
-    ]
     return (
         <>
-            <section className="ImageGalleryContainer">
-                {/* <Row>
-                    <Col lg={12} md={24}>
-                        <div className="StoryGrayContainer">
-                            <h1>STORY</h1>
-                        </div>
-                    </Col>
-                    <Col lg={12} md={24}>
-                        <div className="RightSideImageContainerGray">
-                            <h1>IMAGE</h1>
-                        </div>
-                    </Col>
-                </Row> */}
-                {/* <div>
-                    <p style={{ marginBottom: "0px", textAlign: "center" }}>@SEEDOToys</p>
-                    <h2 style={{ marginTop: "0px", textAlign: "center" }}>We're on Instagram!</h2>
-                </div>
-                <br />
-                <div className="ImagesContainer">
-                    <Row>
-                        {ImagesGalleryData.map((item, index) => (
-                            <Col lg={4} md={6} key={index}>
-                                <div className="GalleryCardImage">
-                                    <img src={item} alt={`Gallery Image ${index + 1}`} />
-                                </div>
-                            </Col>
-                        ))}
-
-                    </Row>
-                    <br />
-                    <div style={{ display: "flex", justifyContent: "center", width: "100%" }} className="InsideContainer">
-                        <Link to="https://www.instagram.com/seedotoys/" target="_blank"><button><FaArrowRightLong /></button></Link>
-                    </div>
-                </div> */}
-            </section>
+           
 
 
             <section className="OurBestsellers" >
@@ -121,10 +42,10 @@ const ImagesGallery = () => {
                             spaceBetween={30}
                             freeMode={true}
                             loop={true}
-                            // autoplay={{
-                            //     delay: 2000,
-                            //     disableOnInteraction: false,
-                            // }}
+                            autoplay={{
+                                delay: 2000,
+                                disableOnInteraction: false,
+                            }}
                             navigation={true}
                             speed={700}
                             modules={[Autoplay, FreeMode, Navigation, Pagination]}
@@ -279,27 +200,7 @@ const ImagesGallery = () => {
                 </div>
 
             </section>
-            {/* <section className="OurBestsellers">
-                <div className="OurServicesSection" style={{ padding: "0px" }}>
-                    <div className="headingHeaderContainer">
-                     
-                    </div>
-                    <Row>
-                        <Col lg={12}>
-                            <div>
-                                <h1 >Certifications </h1>
-                                <p style={{ fontSize: "18px" }}>As strong advocates for local manufacturing, we take pride in sourcing all our components from within India. We fully support and align with Mr. Narendra Modi's visions of promoting the “Make In India" initiative, fostering economic
-                                    growth and self-reliance in our nation.</p>
-                            </div>
-                        </Col>
-                        <Col lg={12}>
-                            <div>
-                                <img src={MakeinIndia} alt="" style={{ width: "80%" }} />
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
-            </section> */}
+            
         </>
     )
 }
